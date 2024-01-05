6131a245d36ec98c05498617a9f808ce0a2fe4bc
Arthur Felipe <130934983+ArthurF2023@users.noreply.github.com>
2024-01-04 23:30:32 -0300

    Add files via upload

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada");
+
+            Console.WriteLine("Tabuada 0");
+            Console.WriteLine("0x1");
+            Console.WriteLine("0x2");
+            Console.WriteLine("0x3");
+            Console.WriteLine("0x4");
+            Console.WriteLine("0x5");
+            Console.WriteLine("0x6");
+            Console.WriteLine("0x7");
+            Console.WriteLine("0x8");
+            Console.WriteLine("0x9");
+            Console.WriteLine("0x10");
+
+            Console.WriteLine("Tabuada 1");
+            Console.WriteLine("1x1");
+            Console.WriteLine("1x2");
+            Console.WriteLine("1x3");
+            Console.WriteLine("1x4");
+            Console.WriteLine("1x5");
+            Console.WriteLine("1x6");
+            Console.WriteLine("1x7");
+            Console.WriteLine("1x8");
+            Console.WriteLine("1x9");
+            Console.WriteLine("1x10");
+
+
+            Console.WriteLine("Tabuada 2");
+            Console.WriteLine("2x1");
+            Console.WriteLine("2x2");
+            Console.WriteLine("2x3");
+            Console.WriteLine("2x4");
+            Console.WriteLine("2x5");
+            Console.WriteLine("2x6");
+            Console.WriteLine("2x7");
+            Console.WriteLine("2x8");
+            Console.WriteLine("2x9");
+            Console.WriteLine("2x10");
+
+            Console.WriteLine("Tabuada 3");
+            Console.WriteLine("3x1");
+            Console.WriteLine("3x2");
+            Console.WriteLine("3x3");
+            Console.WriteLine("3x4");
+            Console.WriteLine("3x5");
+            Console.WriteLine("3x6");
+            Console.WriteLine("3x7");
+            Console.WriteLine("3x8");
+            Console.WriteLine("3x9");
+            Console.WriteLine("3x10");
+
+            Console.WriteLine("Tabuada 4");
+            Console.WriteLine("4x1");
+            Console.WriteLine("4x2");
+            Console.WriteLine("4x3");
+            Console.WriteLine("4x4");
+            Console.WriteLine("4x5");
+            Console.WriteLine("4x6");
+            Console.WriteLine("4x7");
+            Console.WriteLine("4x8");
+            Console.WriteLine("4x9");
+            Console.WriteLine("4x10");
+
+            Console.WriteLine("Tabuada 5");
+            Console.WriteLine("5x1");
+            Console.WriteLine("5x2");
+            Console.WriteLine("5x3");
+            Console.WriteLine("5x4");
+            Console.WriteLine("5x5");
+            Console.WriteLine("5x6");
+            Console.WriteLine("5x7");
+            Console.WriteLine("5x8");
+            Console.WriteLine("5x9");
+            Console.WriteLine("5x10");
+
+
+            Console.WriteLine("Tabuada 6");
+            Console.WriteLine("6x1");
+            Console.WriteLine("6x2");
+            Console.WriteLine("6x3");
+            Console.WriteLine("6x4");
+            Console.WriteLine("6x5");
+            Console.WriteLine("6x6");
+            Console.WriteLine("6x7");
+            Console.WriteLine("6x8");
+            Console.WriteLine("6x9");
+            Console.WriteLine("6x10");
+
+
+            Console.WriteLine("Tabuada 7");
+            Console.WriteLine("7x1");
+            Console.WriteLine("7x2");
+            Console.WriteLine("7x3");
+            Console.WriteLine("7x4");
+            Console.WriteLine("7x5");
+            Console.WriteLine("7x6");
+            Console.WriteLine("7x7");
+            Console.WriteLine("7x8");
+            Console.WriteLine("7x9");
+            Console.WriteLine("7x10");
+
+
+
+            Console.WriteLine("Tabuada 8");
+            Console.WriteLine("8x1");
+            Console.WriteLine("8x2");
+            Console.WriteLine("8x3");
+            Console.WriteLine("8x4");
+            Console.WriteLine("8x5");
+            Console.WriteLine("8x6");
+            Console.WriteLine("8x7");
+            Console.WriteLine("8x8");
+            Console.WriteLine("8x9");
+            Console.WriteLine("8x10");
+
+
+            Console.WriteLine("Tabuada 9");
+            Console.WriteLine("9x1");
+            Console.WriteLine("9x2");
+            Console.WriteLine("9x3");
+            Console.WriteLine("9x4");
+            Console.WriteLine("9x5");
+            Console.WriteLine("9x6");
+            Console.WriteLine("9x7");
+            Console.WriteLine("9x8");
+            Console.WriteLine("9x9");
+            Console.WriteLine("9x10");
+
+
+            Console.WriteLine("Tabuada 10");
+            Console.WriteLine("10x1");
+            Console.WriteLine("10x2");
+            Console.WriteLine("10x3");
+            Console.WriteLine("10x4");
+            Console.WriteLine("10x5");
+            Console.WriteLine("10x6");
+            Console.WriteLine("10x7");
+            Console.WriteLine("10x8");
+            Console.WriteLine("10x9");
+            Console.WriteLine("10x10");
+
+
+            Console.ReadLine();
+        }
+    }
+}
