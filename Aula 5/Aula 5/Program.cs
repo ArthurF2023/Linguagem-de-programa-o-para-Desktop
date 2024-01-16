9db16672b1eaf33b0d162362d4f0f16423f979fc
Arthur Felipe <130934983+ArthurF2023@users.noreply.github.com>
2024-01-16 19:47:56 -0300

    Add files via upload

---
+++ Aula 5/Aula 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //String seria um em texto piloto seria a variavel
+          string[] pilotos = new string[4]
+          {"Ayrton Senna", "Michael Schumacher", "Lewis Hamilton", "Alain Prost" };
+          
+          //Pilotos [2] vai mostra na tela WriteLine sequencia que foi definida no pilotos[2]
+          Console.WriteLine(pilotos[2]);
+          Console.WriteLine();
+          pilotos[3] = "Rubens Barrichello";
+
+          //FORACH esta juntando string que texto junto 
+          foreach (string piloto in pilotos) 
+          Console.WriteLine(piloto);
+          Console.ReadLine();
+
+         //Esse codigo esta realizando de mostra na tela so Corredores de forma F1, quando vc altera no piloto[] muda ordem
+         //que vai mosta na tela 
+          
+
+        }
+    }
+}
